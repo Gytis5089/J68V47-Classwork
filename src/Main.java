da151058be431846d6d0ab68f59050b4faa48abc
Gytis5089 <gytismitkevicius@gmail.com>
2023-09-01 11:56:41 +0100

    Haikus

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        //comment
-        System.out.println("Hello, World!");
+        //hakiuwus
+        String line1 = "Autumn leaves descend,\n" + "Whisper secrets to the earth,\n" + "Nature's tapestry.";
+        String line2 = "Moonlight on the waves,\n" + "Silver threads on dark waters,\n" + "Night's serenade sings.";
+        String line3 = "Blossoms greet the dawn,\n" + "Petals blush with morning light,\n" + "Life's sweet symphony.";
+        //prints
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
     }
 }
