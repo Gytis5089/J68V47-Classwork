5df051fb907df1b724efad5ba038127f793f6cae
Gytis5089 <gytismitkevicius@gmail.com>
2023-09-01 11:57:23 +0100

    Haikus

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         //hakiuwus
-        String line1 = "Autumn leaves descend,\n" + "Whisper secrets to the earth,\n" + "Nature's tapestry.";
-        String line2 = "Moonlight on the waves,\n" + "Silver threads on dark waters,\n" + "Night's serenade sings.";
-        String line3 = "Blossoms greet the dawn,\n" + "Petals blush with morning light,\n" + "Life's sweet symphony.";
+        String line1 = "Autumn leaves descend,\nWhisper secrets to the earth,\nNature's tapestry.";
+        String line2 = "Moonlight on the waves,\nSilver threads on dark waters,\nNight's serenade sings.";
+        String line3 = "Blossoms greet the dawn,\nPetals blush with morning light,\nLife's sweet symphony.";
         //prints
         System.out.println(line1);
         System.out.println(line2);
