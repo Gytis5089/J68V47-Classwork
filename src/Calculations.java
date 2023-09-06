019971afa471623269e03319195a08edaae5e484
Gytis5089 <gytismitkevicius@gmail.com>
2023-09-06 15:30:32 +0100

    Accurate Division (Used to be int division, now includes flats (Casting)) + QOL

--- src/Calculations.java
+++ src/Calculations.java
@@ -10,15 +10,17 @@ public class Calculations {
         System.out.print("Number 2:");
         Integer num2 = input.nextInt();
 
+        System.out.println("");
+
         System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
         System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
         System.out.println(num2 + " - " + num1 + " = " + (num2 - num1));
         System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
-        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
-        System.out.println(num2 + " / " + num1 + " = " + (num2 / num1));
+        System.out.println(num1 + " / " + num2 + " = " + ((float) num1 / (float) num2));
+        System.out.println(num2 + " / " + num1 + " = " + ((float) num2 / (float) num1));
         System.out.println(num1 + " % " + num2 + " = " + (num1 % num2));
         System.out.println(num2 + " % " + num1 + " = " + (num2 % num1));
-        System.out.println(num1 + " ^ " + num2 + " = " + (Math.round(Math.pow(num1, num2))));
-        System.out.println(num2 + " ^ " + num1 + " = " + (Math.round(Math.pow(num2, num1))));
+        System.out.println(num1 + " ^ " + num2 + " = " + ((int) Math.pow(num1, num2)));
+        System.out.println(num2 + " ^ " + num1 + " = " + ((int) Math.pow(num2, num1)));
     }
 }
